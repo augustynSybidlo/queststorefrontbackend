8375aa07fba97119edc7c88c38f3a0097497c6ee
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-04-17 11:54:57 +0200

    Create server in main class

--- src/main/java/Application/Application.java
+++ src/main/java/Application/Application.java
@@ -1,16 +1,26 @@
 package Application;
 
 import Controllers.LoginController;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
 public class Application{
 
-    public static void main (String[] argv){
-        Application app = new Application();
-        app.startApp();
+    public static void main (String[] argv) throws Exception{
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/login", new LoginController());
+        server.setExecutor(null);
+        server.start();
     }
 
-    private void startApp(){
-        LoginController loginProcedure = new LoginController();
-        loginProcedure.login();
-    }
+
+
+//    private void startApp(){
+//        LoginController loginProcedure = new LoginController();
+//        loginProcedure.login();
+//    }
 }
